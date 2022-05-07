7d19ca6d7171a3e825ddff1ee1ce8aa1d979c77b
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-05-06 21:09:58 -0400

    Fixing the layout

--- ex01.php
+++ ex01.php
@@ -1,4 +1,7 @@
 <?php
 
-// 1. Write a PHP script to get the PHP version and configuration information
+echo "1. Write a PHP script to get the PHP version and configuration information <br/><br/>";
+echo "<b>Solution:</b><br/><br/>";
+
 echo phpinfo();
+?>
---
+++ ex02.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "2. Write a PHP script to display the following strings. Go to the editor"."<br/>";
+echo "Sample String :"."<br/>";
+echo "Tomorrow I \'ll learn PHP global variables."."<br/>";
+echo "This is a bad command : del c:\\*.*"."<br/>";
+echo "Expected Output :"."<br/>";
+echo "Tomorrow I 'll learn PHP global variables."."<br/>";
+echo "This is a bad command : del c:\*.* "."<br/><br/>";
+
+echo "<b>Solution:</b><br/><br/>";
+
+echo "Tomorrow I \'ll learn PHP global variables."."<br/>";
+echo "This is a bad command : del c:\\*.*"."<br/>";
+
+?>
