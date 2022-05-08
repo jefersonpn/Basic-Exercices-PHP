991c799c593da38dd4c668443496215ce89e1522
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-05-07 22:15:15 -0400

    Add form

--- FunctionSubstr.php
+++ FunctionSubstr.php
@@ -17,6 +17,27 @@
         }
     }
 
-   echo verticalName("Wellen");  
+   //echo verticalName("Wellen");  
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Exercise 04</title>
+</head>
+<body>
+    <div class="ms-1">
+    <h3> Faça seu nome na Vertical</h3>
+    <form method="post">
+        <input type="text" name="name">
+        <input type="submit" value="Submit Name">
+    </form>
+    </div>
+    <br/>
+    <p class="d-flex justify-content-center"><?php echo verticalName($_POST['name']); ?></p>  
+    </body>
+</html>
